4aa8d6d124083624ffd6a6b605778e45ee8408f5
pnhung177 <pnhung177@acegik.net>
2017-01-10 13:48:30 +0700

    Provide debug & loadsync libraries

---
+++ index.js
@@ -0,0 +1,4 @@
+global.geotool = global.geotool || {};
+global.geotool.debug = global.geotool.debug || require('./lib/debug');
+global.geotool.loadsync = global.geotool.loadsync || require('loadsync');
+module.exports = global.geotool;
